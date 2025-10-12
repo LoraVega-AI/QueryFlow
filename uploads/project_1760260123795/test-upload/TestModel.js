ead6c2de28205213d1237e4f3c51433f84e30b47
Lora Vega <vegalora70@gmail.com>
2025-10-12 12:55:19 +0200

    test files deleted

--- uploads/project_1760260123795/test-upload/TestModel.js
+++
@@ -1,9 +0,0 @@
-
-const { DataTypes } = require('sequelize');
-module.exports = (sequelize) => {
-  const TestModel = sequelize.define('TestModel', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING(100), allowNull: false }
-  });
-  return TestModel;
-};
